3a4c7de7bd98a649f7973c6db154083fd468128f
takashithiba <k021g1120@g.neec.ac.jp>
2022-04-13 13:01:04 +0900

    01_01終了

--- DirectXGeme/main.cpp
+++ DirectXGeme/main.cpp
@@ -33,5 +33,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	RECT wrc = { 0,0,window_width,window_heigit };
 	//自動サイズを補正する
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	//ウインドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名
+		L"DirectXGame",//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,//標準的なウインドウスタイル
+		CW_USEDEFAULT,//表示X座標（OSに任せる）
+		CW_USEDEFAULT,//表示Y座標（OSに任せる）
+		wrc.right - wrc.left,//ウインドウ横幅
+		wrc.bottom - wrc.top,//ウインドウ縦幅
+		nullptr,//親ウインドウハンドル
+		nullptr,//メニューハンドル
+		w.hInstance,//呼び出しアプリケーションハンドル
+		nullptr);//オプション
+
+	//ウインドウを表示状態にする
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+
+	//DirectX初期化処理　ここから
+	// 
+	//DirectX初期化処理　ここまで
+
+	//ゲームループ
+	while (true) {
+		//メッセージがある？
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);//キー入力メッセージの処理
+			DispatchMessage(&msg);//プロシージャにメッセージが残る
+		}
+
+		//×ボタンで終了メッセージがきたらゲームループを抜ける
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+		//DirectX毎フレーム処理　ここから
+		
+		//DirectX毎フレーム処理　ここまで
+	}
+
+	//ウインドウクラスを登録解除
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
 	return 0;
 }
